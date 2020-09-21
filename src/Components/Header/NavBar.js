494fb4909b72a2ae8c5e06325db0af08a2c08c85
Ghiurco Gabriel <ghiurcogabriel@gmail.com>
2020-09-21 22:29:12 +0300

    route

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -3,32 +3,10 @@ import DropdownMenu from '../Main-up-section/DropdownMenu';
 import clapperboard from './clapperboard.svg';
 import Searchbar from './Searchbar';
 import LoginButton from './LoginButton';
-import axios from "axios";
+import {Link} from "react-router-dom";
 import "./../../App.css";
 
 export default class NavBar extends Component {
-
-        constructor(props) {
-            super(props);
-        
-            this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
-            this.handleLogoutClick = this.handleLogoutClick.bind(this);
-          }        
-
-    handleSuccessfulAuth(data) {
-        this.props.handleLogin(data);
-    }
-
-    handleLogoutClick() {
-        axios
-          .delete("http://localhost:3001/logout", { withCredentials: true })
-          .then(response => {
-            this.props.handleLogout();
-            })
-          .catch(error => {
-            console.log("logout error", error);
-            });
-      }
     render() {
         return (
             <div className='main-container'>
@@ -37,7 +15,10 @@ export default class NavBar extends Component {
                 <img className='theater' src={clapperboard} alt='batman'/>
                 <DropdownMenu />
                 <Searchbar />
-                <LoginButton />
+                <Link to='/Login' style={{ textDecoration: "none" }}>
+                  <LoginButton />                
+                </Link>
+
                </div>
             </div>
           </div>
